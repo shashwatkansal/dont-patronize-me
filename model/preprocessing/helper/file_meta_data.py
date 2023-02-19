be6362d5c4156d39cdb082956625b80232f014cd
mrkobhrai <40830901+mrkobhrai@users.noreply.github.com>
2023-02-19 22:56:24 +0000

    Comment file metadata

--- model/preprocessing/helper/file_meta_data.py
+++ model/preprocessing/helper/file_meta_data.py
@@ -1,25 +1,43 @@
+'''
+The file where the raw DPM PCL data is stored
+'''
 PCL_DATA_FILE_META = {
     'filepath': './data/raw/dontpatronizeme_pcl.tsv',
     'columns':['par_id', 'art_id', 'keyword', 'country_code', 'text', 'label']
 }
 
+'''
+The file where the raw DPM category data is stored
+'''
 CATEGORIES_DATA_FILE_META = {
     'filepath': './data/raw/dontpatronizeme_categories.tsv',
     'columns':['par_id', 'art_id', 'text', 'keyword', 'country_code', 'span_start', 'span_finish', 'span_text', 'pcl_category', 'number_of_annotators']
 }
 
+'''
+The file where the training data split by paragraph id (par_id) is stored
+'''
 TRAIN_SPLIT_FILE_META = {
     'filepath': './data/splits/train_semeval_parids-labels.csv',
 }
 
+'''
+The file where the dev data split by paragraph id (par_id) is stored
+'''
 OFFICIAL_DEV_SPLIT_FILE_META = {
     'filepath': './data/splits/dev_semeval_parids-labels.csv'
 }
 
+'''
+The file where the dev pcl data is stored
+'''
 OFFICIAL_DEV_PCL_DATA_FILE_META = {
     'filepath': './data/dontpatronizeme_pcl_dev.tsv'
 }
 
+'''
+The file where the training pcl data is stored
+'''
 TRAINING_PCL_DATA_FILE_META = {
     'filepath': './data/dontpatronizeme_pcl_training.tsv'
 }
--- model/preprocessing/helper/files_helper.py
+++ model/preprocessing/helper/files_helper.py
@@ -9,15 +9,20 @@ def read_raw_datafile(file_meta: dict) -> pd.DataFrame:
     return df
 
 '''
-Read the CSV data file
+Read the CSV data indices split file
 '''
 def read_split(file_meta: dict) -> pd.DataFrame:
-    df = pd.read_csv(file_meta['filepath'])
-    return df
+    return pd.read_csv(file_meta['filepath'])
 
 '''
-Print a DF to a TSV file
+Print a dataframe to a TSV file
 '''
 def write_dataframe(file_meta: dict, dataframe: pd.DataFrame)->None:
     print("Writing to TSV:", file_meta['filepath'])
     dataframe.to_csv(file_meta['filepath'], sep='\t', index=False)
+    
+'''
+Read a dataframe from a TSV file
+'''
+def read_raw_datafile(file_meta: dict) -> pd.DataFrame:
+    return pd.read_csv(file_meta['filepath'], sep='\t')
